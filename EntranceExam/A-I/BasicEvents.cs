3adea6d26c46ded237c31d29180acf5a155d15da
Aklilu Weldemihret Weldu <akliluweldu@gmail.com>
2021-06-17 19:36:13 +0200

    basic folder structure

---
+++ EntranceExam/A-I/BasicEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntranceExam
+{
+    public class BasicEvents
+    {
+        public event EventHandler Channel;
+        ConsoleKeyInfo info;
+        public void ears()
+        {
+            Channel += Speakers;
+        }
+
+        private void Speakers(object sender, EventArgs e)
+        {
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine("I heard something!");
+                if (info.Key == ConsoleKey.X)
+                    break;
+            }
+            }
+
+        public void Ears()
+        {
+          
+            if (info.Key == ConsoleKey.Spacebar)
+                {
+                    Channel(this, EventArgs.Empty);
+                }
+        }
+
+    }
+}
--- EntranceExam/Program.cs
+++ EntranceExam/Program.cs
@@ -7,15 +7,17 @@ namespace EntranceExam
       
         static void Main(string[] args)
         {
-            DelegatingClass delegating = new DelegatingClass();
-            DelegatingClass.Greet delegatingClass = new DelegatingClass.Greet(delegating.messageOne);
-            DelegatingClass.Greet delegatingClass2 = new DelegatingClass.Greet(delegating.message2);
-            DelegatingClass.Greet delegatingClass3 = new DelegatingClass.Greet(delegating.message3);
+            //DelegatingClass delegating = new DelegatingClass();
+            //DelegatingClass.Greet delegatingClass = new DelegatingClass.Greet(delegating.messageOne);
+            //DelegatingClass.Greet delegatingClass2 = new DelegatingClass.Greet(delegating.message2);
+            //DelegatingClass.Greet delegatingClass3 = new DelegatingClass.Greet(delegating.message3);
 
-            //Invocation 
-            delegatingClass();
-            delegatingClass2();
-            delegatingClass3();
+            ////Invocation 
+            //delegatingClass();
+            //delegatingClass2();
+            //delegatingClass3();
+            BasicEvents basic = new BasicEvents();
+            basic.Ears();
 
         }
     }
